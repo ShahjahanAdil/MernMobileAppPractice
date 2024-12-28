29592e96af891c4301d315096de38d90b19e25bd
ShahjahanAdil <shahjahanadil01@gmail.com>
2024-12-28 12:59:50 +0500

    Todos length message function done

--- client/src/screens/Frontend/AllTodos.js
+++ client/src/screens/Frontend/AllTodos.js
@@ -55,33 +55,40 @@ export default function AllTodo({ navigation }) {
                         <Text style={{ color: '#666', fontSize: 16, fontWeight: '600', marginTop: 5, marginBottom: 12 }}>Manage all your todos here:</Text>
                     </View>
                     {
-                        todos.map(todo => {
-                            return (
-                                <View style={styles.todoBox} key={todo.todoId}>
-                                    <View style={styles.todoBoxTop}>
-                                        <Text style={{ color: '#666', fontWeight: 600 }}>{todo.title}</Text>
+                        todos.length > 0 ?
+                            todos.map(todo => {
+                                return (
+                                    <View style={styles.todoBox} key={todo.todoId}>
+                                        <View style={styles.todoBoxTop}>
+                                            <Text style={{ color: '#666', fontWeight: 600 }}>{todo.title}</Text>
+                                        </View>
+                                        <View style={styles.todoBoxMid}>
+                                            <Text style={{ color: '#888' }}>{todo.description}</Text>
+                                        </View>
+                                        <View style={styles.todoBoxBottom}>
+                                            <TouchableOpacity style={{ flex: 1 }} onPress={() => navigation.navigate('TodoDetails', { todo })}>
+                                                <View style={{ flex: 1, alignItems: 'center', borderRightWidth: 1, borderColor: '#666' }}>
+                                                    <FeatherIcon name='edit-2' size={16} color='#0c82bd' />
+                                                </View>
+                                            </TouchableOpacity>
+                                            <TouchableOpacity style={{ flex: 1 }} onPress={() => handleDeleteConfirmation(todo.todoID)}>
+                                                <View style={{ flex: 1, alignItems: 'center' }}>
+                                                    <FeatherIcon name='trash' size={16} color='#ff3131' />
+                                                </View>
+                                            </TouchableOpacity>
+                                        </View>
+                                        <View style={styles.todoStatus}>
+                                            <Text style={{ color: todo.status === 'complete' ? '#08ff00' : '#e1f309' }}>{todo.status}</Text>
+                                        </View>
                                     </View>
-                                    <View style={styles.todoBoxMid}>
-                                        <Text style={{ color: '#888' }}>{todo.description}</Text>
-                                    </View>
-                                    <View style={styles.todoBoxBottom}>
-                                        <TouchableOpacity style={{ flex: 1 }} onPress={() => navigation.navigate('TodoDetails', { todo })}>
-                                            <View style={{ flex: 1, alignItems: 'center', borderRightWidth: 1, borderColor: '#666' }}>
-                                                <FeatherIcon name='edit-2' size={16} color='#0c82bd' />
-                                            </View>
-                                        </TouchableOpacity>
-                                        <TouchableOpacity style={{ flex: 1 }} onPress={() => handleDeleteConfirmation(todo.todoID)}>
-                                            <View style={{ flex: 1, alignItems: 'center' }}>
-                                                <FeatherIcon name='trash' size={16} color='#ff3131' />
-                                            </View>
-                                        </TouchableOpacity>
-                                    </View>
-                                    <View style={styles.todoStatus}>
-                                        <Text style={{ color: todo.status === 'complete' ? '#08ff00' : '#e1f309' }}>{todo.status}</Text>
-                                    </View>
-                                </View>
-                            )
-                        })
+                                )
+                            }) :
+                            <View style={styles.todosMessage}>
+                                <Text style={{ color: '#f35e5e' }}>You have no todos.</Text>
+                                <TouchableOpacity onPress={() => navigation.navigate('CreateTodo')}>
+                                    <Text style={{ color: '#f35e5e', textDecorationLine: 'underline' }}>Create todo now!</Text>
+                                </TouchableOpacity>
+                            </View>
                     }
                 </View>
             </ScrollView>
@@ -134,5 +141,14 @@ const styles = StyleSheet.create({
         paddingHorizontal: 15,
         borderTopLeftRadius: 8,
         borderTopRightRadius: 8,
+    },
+    todosMessage: {
+        padding: 10,
+        backgroundColor: '#fde3e3',
+        borderWidth: 1,
+        borderColor: '#f35e5e',
+        borderRadius: 8,
+        flexDirection: 'row',
+        gap: 3
     }
 })
